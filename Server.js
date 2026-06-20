aed9456f2f4d444e7a8a419576c76a3259a190c5
Akshithapoojari <24wh1a05c4@bvrithyderabad.edu.in>
2026-06-20 09:54:15 +0530

    added card.html and updated css code

--- Server.js
+++ Server.js
@@ -1,8 +1,10 @@
-const express =require("express");
-const app =express();
-const port =process.env.PORT||6700;
-//to tell app where to find the  static files (html,css,js) stored;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 6700;
+
+//To tell the app where static files - html/css/js are stored
 app.use(express.static("frontend"));
-app.listen(port,function(){
-    console.log("App running on http://localhost:"+port);
+
+app.listen(port , function(){
+    console.log("App running on http://localhost:" + port);
 });
